457fff6895659e06258b1d97bacef2a74a5630a0
Thomas-Vargas <tavargas2012irc@gmail.com>
2023-03-12 12:25:31 -0500

    Cleaned up code, moved css to its own file, added comments and updated README.

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,24 +4,23 @@ function onReady() {
     console.log(`jq loaded`);
     getToDoList();
     $('#submit-to-do').on('click', addNewToDo);
+    $('#view-to-dos').on('click', '.markCompleteBtn', markAsComplete);
 
+    // Show modal when deleteBtn is clicked and update the modal deleteBtn with idToDelete
     $('#view-to-dos').on('click', '.deleteBtn', function() {
         $('.modal').modal('toggle');
         const idToDelete = $(this).parent().parent().data().id;
-        console.log(idToDelete);
-      
         $('#modal-btn-delete').data('idToDelete', idToDelete);
     });
-      
+    
+    // Listener for modal deleteBtn, closes modal when finished
     $(document).on('click', '#modal-btn-delete', function() {
         const idToDelete = $(this).data('idToDelete');
-        console.log(idToDelete);
         deleteToDo(idToDelete);
         $('.modal').modal('hide');
     });
       
-
-    $('#view-to-dos').on('click', '.markCompleteBtn', markAsComplete);
+    // Allows user to use enter key in input
     $('input').on('keypress', function(e) {
         let key = e.which;
         if (key === 13) {
@@ -42,19 +41,19 @@ function markAsComplete() {
         getToDoList();
     }).catch((error) => {
         console.log('error updating to-do', error);
-    })
+    });
 }
 
 function deleteToDo(idToDelete) {
     $.ajax({
         type: "DELETE",
         url: `/to-do/deleteToDo/${idToDelete}`
-      }).then((result) => {
+    }).then((result) => {
         console.log('successfully deleted', idToDelete);
         getToDoList();
-      }).catch((error) => {
+    }).catch((error) => {
         console.log('error deleting to-do', error);
-      })
+    });
 }
 
 function getToDoList() {
@@ -80,16 +79,14 @@ function addNewToDo() {
                 task: $('#to-do-input').val(),
                 isComplete: false
             }
-           })
-           .then((response) => {
-                console.log('response from server', response);
-                getToDoList();
-           })
-           .catch((error) => {
-                console.log('error in post', error);
-           })
+        }).then((response) => {
+            console.log('response from server', response);
+            getToDoList();
+        }).catch((error) => {
+            console.log('error in post', error);
+        })
     
-           $('#to-do-input').val('');
+        $('#to-do-input').val('');
     }
     else {
         alert('Please fill out input!')
@@ -101,30 +98,34 @@ function renderToDos(toDoList) {
 
     let color = '';
     let status = 'Incomplete';
+
     for(let task of toDoList) {
+        // Include time task was completed if isComplete = true
         if (task.isComplete) {
+            // Update color and status variables 
             color = 'green';
             status = 'Complete'
 
 
             $('#view-to-dos').append(`
-            <tr data-id='${task.id}' class="${color}">
-                <th>${task.task}</th>
-                <th>${status}</th>
-                <th>Task completed on ${task.timeCompleted}</th>
-                <th><button class='deleteBtn btn btn-danger'>Delete</button></th>
-            </tr>
-        `); 
+                <tr data-id='${task.id}' class="${color}">
+                    <th>${task.task}</th>
+                    <th>${status}</th>
+                    <th>Task completed on ${task.timeCompleted}</th>
+                    <th><button class='deleteBtn btn btn-danger'>Delete</button></th>
+                </tr>
+            `); 
         }
         else {
+            // Else include button to mark task as complete
             $('#view-to-dos').append(`
-            <tr data-id='${task.id}' class="${color}">
-                <th>${task.task}</th>
-                <th>${status}</th>
-                <th><button class='markCompleteBtn btn btn-success'>Complete</button></th>
-                <th><button class='btn btn-danger deleteBtn'>Delete</button></th>
-            </tr>
-        `); 
+                <tr data-id='${task.id}' class="${color}">
+                    <th>${task.task}</th>
+                    <th>${status}</th>
+                    <th><button class='markCompleteBtn btn btn-success'>Complete</button></th>
+                    <th><button class='btn btn-danger deleteBtn'>Delete</button></th>
+                </tr>
+            `); 
         }
     }
 }
