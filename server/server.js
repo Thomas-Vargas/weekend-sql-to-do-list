3afc910e8f7ae6ae49f43d5ec7fead93955d62e6
Thomas-Vargas <tavargas2012irc@gmail.com>
2023-03-13 16:52:45 -0500

    changed port.

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const PORT = 5000 || process.env.PORT;
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('server/public'));
@@ -11,6 +10,7 @@ const toDoRouter = require('./routes/to-do.router');
 
 app.use('/to-do', toDoRouter);
 
+const PORT = process.env.PORT || 5000;
 // Start listening for requests on a specific port
 app.listen(PORT, () => {
   console.log('listening on port', PORT);
